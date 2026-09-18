66a63803e72e767fc5a49576175a3966d3a9f36d
abdulhamidongithub <1997abdulhamid@gmail.com>
2026-09-18 15:59:02 +0500

    Small fixes

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,11 +1,20 @@
+from django import forms
+
+from .models import *
+
 class BookingForm(forms.ModelForm):
+    guest_phone = forms.CharField(
+        label="Telefon raqami",
+        initial="+998 ",
+        widget=forms.TextInput(attrs={"placeholder": "+998 90 123 45 67"}),
+    )
+
     class Meta:
         model = Booking
         fields = ["room", "guest_name", "guest_phone", "date", "total_price", "prepaid_amount", "notes"]
         labels = {
             "room": "Xona",
             "guest_name": "Mehmon ismi",
-            "guest_phone": "Telefon raqami",
             "date": "Sana",
             "total_price": "Umumiy summa",
             "prepaid_amount": "Oldindan to'lov",
